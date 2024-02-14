e9899e1ea98a68541fcbfabd85711ce0334b05aa
caldardn <caldardn@gmail.com>
2024-02-14 11:20:50 -0500

    adding schema

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('@apollo/server');
+
+const { typeDefs, resolvers } = require('./schemas');
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,8 +22,20 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+      db.once('open', () => {
+         app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+      });
+    }
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
+
+
+// app.use(routes);
+
+// db.once('open', () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
